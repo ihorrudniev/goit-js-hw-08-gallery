46b94c396e8ec58b6d70f70ff1b1dcac5d83e114
Ihor Rudniev <rudnievihor@gmail.com>
2021-07-28 12:44:51 +0300

    Update main-js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import galleryItems from "./app.js";
 
-// console.log(galleryItems);
-
 const galleryRef = document.querySelector(".js-gallery");
 const lightboxRef = document.querySelector(".js-lightbox");
 const lightboxOverlayRef = document.querySelector(".lightbox__overlay");
@@ -9,12 +7,8 @@ const lightboxContentRef = document.querySelector(".lightbox__content");
 const lightboxImageRef = document.querySelector(".lightbox__image");
 const lightboxButtonRef = document.querySelector(".lightbox__button");
 
+// create dinamic elemens HTML
 const cardsMarkup = createGallery(galleryItems);
-// console.log(cardsMarkup);
-galleryRef.insertAdjacentHTML("beforeend", cardsMarkup);
-
-galleryRef.addEventListener("click", onGalleryRefClick);
-
 function createGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -29,6 +23,7 @@ function createGallery(galleryItems) {
       src="${preview}"
       data-source="${original}"
       alt="${description}"
+      tabindex = num.indexOf + 1
     />
   </a>
 </li>
@@ -36,9 +31,50 @@ function createGallery(galleryItems) {
     })
     .join("");
 }
+galleryRef.insertAdjacentHTML("beforeend", cardsMarkup);
 
+// add Events Listeners
+galleryRef.addEventListener("click", onGalleryRefClick);
+lightboxRef.addEventListener("click", onCloseModal);
+document.addEventListener("keydown", onCloseModalAfterEsc);
+
+// Event handler
 function onGalleryRefClick(e) {
-  console.log(e.target);
+  e.preventDefault();
+  const target = e.target;
+  console.log(target);
+  const isTadgetImg = target.classList.contains(".js-gallery");
+
+  if (isTadgetImg) {
+    lightboxRef.classList.add("is-open");
+    lightboxImageRef.setAttribute("src", target);
+    lightboxImageRef.setAttribute("alt", target);
+  }
 }
 
+function onCloseModal(e) {
+  if (e.target.nodeName === "BUTTON") {
+    lightboxEl.classList.toggle("is-open");
+  }
+}
+
+function onCloseModalAfterEsc(e) {}
+
+// helpers function
+
+function closeModal() {}
+
+// function onOpenModal(e) {
+//   // e.preventDefault();
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   lightboxEl.classList.toggle("is-open");
+//   lightboxImageEl.src = e.target.src;
+//   lightboxImageEl.alt = e.target.alt;
+// }
+//--------------- CloseModal--------
+
 // console.log(createGallery(galleryItems));
+
+// =======================================
